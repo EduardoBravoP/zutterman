64cf59a031b2a63f64c106cfda3637f5087c6884
Eduardo Bravo <eduardobravopires@gmail.com>
2021-09-10 18:18:39 -0300

    Feat: Started to work on Home page

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,9 +1,41 @@
-import type { NextPage } from 'next'
+import type { NextPage } from "next";
+import Image from "next/image";
+import Footer from "../components/Footer";
+import Header from "../components/Header";
+
+import styles from "../styles/pages/index.module.scss";
 
 const Home: NextPage = () => {
   return (
-    <h1>Hello World!</h1>
-  )
-}
+    <>
+      <Header />
 
-export default Home
+      <section className={styles.graySection}>
+        <div>
+          <h1>
+            Measuring <br /> everything
+          </h1>
+          <Image
+            alt="measuring tape"
+            src="/MeasuringTape1.png"
+            width={682}
+            height={335}
+          />
+        </div>
+      </section>
+
+      <section className={styles.blueSection}>
+        <div>
+          <h1>
+            We are Leader <br /> in Measure Tapes
+          </h1>
+          <p>There are 5x the circumference of planet earth in metric tapes.</p>
+        </div>
+      </section>
+
+      <Footer />
+    </>
+  );
+};
+
+export default Home;
