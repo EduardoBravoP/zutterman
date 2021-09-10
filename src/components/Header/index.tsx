d57bd465acb06584608080877e74d3651c6f9c82
Eduardo Bravo <eduardobravopires@gmail.com>
2021-09-10 20:18:18 -0300

    Styles: Created Header responsivity

--- src/components/Header/index.tsx
+++ src/components/Header/index.tsx
@@ -5,13 +5,23 @@ import styles from "./styles.module.scss";
 export default function Header() {
   return (
     <header className={styles.container}>
-      <div className={styles.image}>
+      <div className={styles.logo}>
         <Image
           alt="Zutterman"
           src="/Logo.svg"
           width={262}
           height={70}
           layout="responsive"
+        />
+      </div>
+
+      <div className={styles.miniLogo}>
+        <Image
+          alt="Zutterman"
+          src="/MiniLogo.svg"
+          width={40}
+          height={40}
+          className={styles.miniLogo}
         />
       </div>
 
