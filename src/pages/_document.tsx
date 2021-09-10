2aee70febeed757cd1b7a4990d1bfaa229c07d91
Eduardo Bravo <eduardobravopires@gmail.com>
2021-09-10 20:18:35 -0300

    Styles: Created Home responsivity

--- src/pages/_document.tsx
+++ src/pages/_document.tsx
@@ -11,6 +11,8 @@ export default class MyDocument extends Document {
             href="https://fonts.googleapis.com/css2?family=Roboto+Condensed:wght@300;400;700&display=swap"
             rel="stylesheet"
           />
+
+          <meta name="viewport" content="width=device-width, initial-scale=1" />
         </Head>
 
         <body>
